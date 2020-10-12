60b1b1739c711b5ff35d44ab79ff2cbdbec6d338
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-12 21:47:36 +0530

    [Akash] Created MoodAnalyser Program

---
+++ MoodAnalyse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserProblem
+{
+    class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            if (message.ToUpper().Contains("SAD"))
+                return "Sad ";
+            else
+                return "Happy";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace MoodAnalyserProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to mood analyzer portal!");
+            Console.WriteLine("Type how you feeling right now:");
+            string mood = Console.ReadLine();
+            MoodAnalyser moodAnalyser = new MoodAnalyser(mood);
+            Console.WriteLine("Currently you are in: " + moodAnalyser.AnalyseMood());
+        }
+
+    }
+}
